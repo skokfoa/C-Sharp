c85c2f1c58f803afc85be6af2f2f22152b5a506a
Arthur <arthurti0728@gmail.com>
2025-08-05 23:42:23 +0800

    2025/08/05-103 Q1完成

---
+++ 102年/Q3/Q3/Program.cs
@@ -0,0 +1,88 @@
+﻿class project
+{
+    static void Main(string[] args)
+    {
+        List<int> startMove = new List<int>();
+        List<int> endMove = new List<int>();
+        List<int> cost = new List<int>();
+        List<string[]> input = new List<string[]>();
+        int i = new int();
+        while(Console.ReadLine() != null)
+        {
+            input[i] = Console.ReadLine().Split();
+            i++;
+        }
+
+        for(int j = 0; j < input.Count; j++)
+        {
+            startMove.Add(int.Parse(input[j][0]));
+            endMove.Add(int.Parse(input[j][1]));
+            cost.Add(int.Parse(input[j][2]));
+        }
+    }
+    List<Node> solve(List<int> start, List<int> goal, List<int> cost)
+    {
+        Queue<Node> queue = new Queue<Node>();
+        HashSet<int> visited = new HashSet<int>();
+
+        Node start_node = new Node(1, null);
+        int end = 7;
+
+        queue.Enqueue(start_node);
+        visited.Add(start_node.status);
+
+        while(queue.Count > 0)
+        {
+            Node now = queue.Dequeue();
+            if(now.status == end)
+            {
+                return constructPath(now);
+            }
+            List<Node> nextPath = move(now, start, goal);
+            foreach (var path in nextPath)
+            {
+                if (!visited.Add(path))
+                {           
+                    queue.Enqueue(path);
+                    visited.Add(path);
+                }
+            }
+        }
+    }
+
+    List<Node> move(Node now, List<int> start, List<int> goal)
+    {
+        List<Node> move = new List<Node>();
+        for(int i = 0; i < start.Count; i++)
+        {
+            if(now.status == start[i])
+            {
+                move.Add(new Node(goal[i], now));
+            }
+        }
+        return move;
+    }
+
+    List<Node> constructPath(Node current)
+    {
+        List<Node> path = new List<Node>();
+        while(current != null)
+        {
+            path.Add(current);
+            current = current.father;
+        }
+        path.Reverse();
+        return path;
+    }
+}
+
+public class Node
+{
+    public int status;
+    public Node father;
+    public Node(int status, Node father)
+    {
+        this.status = status;
+        this.father = father;
+    }
+}
---
+++ 103年/Q1/Q1/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Q1
+{
+    public partial class Form1 : Form
+    {
+        TextBox[] input = new TextBox[6];
+        public Form1()
+        {
+            InitializeComponent();
+            input = new TextBox[] { textBox1, textBox2, textBox3, textBox4, textBox5, textBox6 };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!checkInput())
+            {
+                textBox7.Text = "無解";
+                return;
+            }
+            double[] a = new double[3];
+            double[] b = new double[3];
+            for (int i = 0; i < input.Length; i++)
+            {
+                if(i < 3)
+                {
+                    a[i] = double.Parse(input[i].Text);
+                }
+                else
+                {
+                    b[i - 3] = double.Parse(input[i].Text);
+                }
+            }
+            double result = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                result += a[i] * b[i];
+            }
+            textBox7.Text = $"在台北市的上班族遲到的機率為:{result}";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!checkInput())
+            {
+                textBox7.Text = "無解";
+                return;
+            }
+            double[] a = new double[3];
+            double[] b = new double[3];
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i < 3)
+                {
+                    a[i] = double.Parse(input[i].Text);
+                }
+                else
+                {
+                    b[i - 3] = double.Parse(input[i].Text);
+                }
+            }
+            double result = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                result += a[i] * b[i];
+            }
+            result = (a[2] * b[2]) / result;
+            textBox7.Text = $"如果已知一個人上班遲到，那他是自己開車的機率為何:{result}";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private bool checkInput()
+        {
+            double[] a = new double[6];
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].Text == "")
+                {
+                    return false;
+                }
+                if (!double.TryParse(input[i].Text, out a[i]))
+                {
+                    return false;
+                }
+
+                if (a[i] < 0 || a[i] > 1)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
